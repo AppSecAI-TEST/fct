3ac4441643045f73ce567979072dc4aa52190595
jon <iread@live.com>
2017-06-29 16:05:26 +0800

    update adminweb

--- web/web-admin/src/main/java/com/fct/web/admin/http/controller/index/IndexController.java
+++ web/web-admin/src/main/java/com/fct/web/admin/http/controller/index/IndexController.java
@@ -12,6 +12,6 @@ public class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(){
-        return "/main/login";
+        return "redirect:/login";
     }
 }

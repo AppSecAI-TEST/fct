3580e8a86d90e30a966afa70809225991994432e
qiujun4417 <qiujun4417@hotmail.com>
2017-07-13 23:04:30 +0800

    add

---
+++ taskservice/src/main/java/com/fct/task/service/StartupApplication.java
@@ -0,0 +1,20 @@
+package com.fct.task.service;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by jon on 2017/6/20.
+ */
+@SpringBootApplication(scanBasePackages = "com.fct.task")
+@EnableScheduling
+public class StartupApplication {
+
+    public static void main(String[] args){
+        SpringApplication application = new SpringApplication(StartupApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
+    }
+}

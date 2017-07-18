f4bb79caa3ebc7a6c2cde61bfb9d5725219d19b0
jon <iread@live.com>
2017-07-18 18:37:36 +0800

    up page title

--- web/web-pay/src/main/java/com/fct/web/pay/http/controller/MobileController.java
+++ web/web-pay/src/main/java/com/fct/web/pay/http/controller/MobileController.java
@@ -12,7 +12,6 @@ import com.fct.mall.interfaces.MallService;
 import com.fct.pay.interfaces.MobilePayService;
 import com.fct.web.pay.http.cache.CacheManager;
 import com.fct.web.pay.utils.Constants;
-import com.fct.web.pay.utils.CryptAES;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

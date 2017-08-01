d1888854e451bc0b9a3fe1e9dededd3b889fb2a8
jon <iread@live.com>
2017-08-01 16:06:45 +0800

    up adminweb

--- memberservice/member-data/src/main/java/com/fct/member/data/repository/MemberAuthRepository.java
+++ memberservice/member-data/src/main/java/com/fct/member/data/repository/MemberAuthRepository.java
@@ -1,7 +1,9 @@
 package com.fct.member.data.repository;
 
 import com.fct.member.data.entity.MemberAuth;
+import com.fct.member.data.entity.MemberBankInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by jon on 2017/5/8.
@@ -9,5 +11,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberAuthRepository extends JpaRepository<MemberAuth, Integer> {
 
     MemberAuth findByMemberIdAndPlatform(Integer memberId,String platform);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM MemberAuth WHERE openId=?1 and platform=?2 limit 1")
+    MemberAuth findOneByOpenId(String openId,String platform);
 
 }
